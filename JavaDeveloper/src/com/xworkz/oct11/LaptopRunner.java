49c5e4e82a06e9f8b14790218d70450bdc8bbfc2
unknown <arunpr.xworkz@gmail.com>
2022-10-12 18:42:33 +0530

    Laptop

---
+++ JavaDeveloper/src/com/xworkz/oct11/LaptopRunner.java
@@ -0,0 +1,14 @@
+package com.xworkz.oct11;
+import com.xworkz.oct11.boot.*;
+public class LaptopRunner {
+	public static void main(String[]args) {
+		Laptop men = new Laptop();
+		
+		System.out.println("men.name");
+		men.name="arun";
+		
+		
+		
+	}
+
+}
---
+++ JavaDeveloper/src/com/xworkz/oct11/boot/Laptop.java
@@ -0,0 +1,7 @@
+package com.xworkz.oct11.boot;
+
+public class Laptop {
+	public String name;
+	System.out.println("enter the name");
+
+}
